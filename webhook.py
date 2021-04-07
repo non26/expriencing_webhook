0bfa87ea0d55d54e0418dee86f45f9b865d74411
Chanon Eiemrod <56479062+non26@users.noreply.github.com>
2021-04-07 23:59:33 +0700

    edits method to methods

--- webhook.py
+++ webhook.py
@@ -6,7 +6,10 @@ app = Flask(__name__)
 def root():
     return "in the root!!"
 
-@app.route("/webhook", method=['POST'])
+@app.route("/webhook", methods=['POST'])
 def webHook():
     print(request.json)
     return Response(status=200)
+
+if __name__ == '__main__':
+    app.run()
